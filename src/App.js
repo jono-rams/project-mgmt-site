98ed83d8d9866649a34ffae507356fd3a367d93a
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-02 19:32:55 -0400

    Navbar and basic route setup with Route guarding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+
+// styles
 import './App.css';
 
+// pages & components
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Project from './pages/project/Project'
+
+// components
+import Navbar from "./components/Navbar";
+import Sidebar from './components/Sidebar';
+
 function App() {
+  const { authIsReady, user } = useAuthContext();
+
   return (
     <div className="App">
-      
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Routes>
+              <Route index element={user ? <Dashboard /> : <Navigate to="/login" replace={true} />} />
+              <Route path='create' element={user ? <Create /> : <Navigate to="/login" replace={true} />} />
+              <Route path='projects/:id' element={user ? <Project /> : <Navigate to="/login" replace={true} />} />
+              <Route path="login" element={!user ? <Login /> : <Navigate to="/" replace={true} />} />
+              <Route path="signup" element={!user ? <Signup /> : <Navigate to="/" replace={true} />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
