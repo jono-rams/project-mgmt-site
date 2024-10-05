7c897a197c14bf7e7fbd480ab069cf52e252ba4f
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-05 18:22:52 -0400

    Show All (and Online) Users

---
+++ src/components/AllUsers.js
@@ -0,0 +1,21 @@
+import { useCollection } from '../hooks/database'
+import Avatar from './Avatar'
+import './AllUsers.css'
+
+export default function AllUsers() {
+  const { documents, error } = useCollection('users')
+
+  return (
+    <div className="user-list">
+      <h2>All Users</h2>
+      {documents && documents.map((user) => (
+        <div key={user.id} className="user-list-item">
+          {user.online && <span className="online-user"></span>}
+          <span>{user.displayName}</span>
+          <Avatar src={user.photoURL} />
+        </div>
+      ))}
+      {error && <div className='error'>{error.message}</div>}
+    </div>
+  )
+}
