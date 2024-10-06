b7fd58ed51a47ab2f99a69ee535add20068ed6e5
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-06 19:46:43 -0400

    Added Filter functionality

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,13 @@
+import ProjectFilter from '../../components/ProjectFilter';
 import ProjectList from '../../components/ProjectList';
 import { useCollection } from '../../hooks/database';
+import { useFilter } from '../../hooks/useFilter';
 
 // styles
-import './Dashboard.css'
+import './Dashboard.css';
 
 export default function Dashboard() {
+  const { currentFilter, changeFilter, filterArray } = useFilter();
   const { documents, error } = useCollection(
     'projects',
     ['status', '!=', 'completed']
@@ -14,7 +17,8 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className='error'>Error: {error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {documents && <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter} />}
+      {documents && <ProjectList projects={filterArray(documents)} />}
     </div>
-  )
+  );
 }
